e35e2df756829afc8eb698e79ac62603d7697c66
Santiago José Dandois <santiago.dandois@widergy.com>
2024-08-21 23:31:38 -0300

    visualizer frontend

--- tasks-war-website/src/main.rs
+++ tasks-war-website/src/main.rs
@@ -1,9 +1,16 @@
 use axum::{response::Html, routing::get, Router};
 
+use tower_http::services::{ServeDir, ServeFile};
+
 #[tokio::main]
 async fn main() {
+    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/foo", get(|| async { "Hi from /foo" }))
+        .nest_service("/assets", serve_dir.clone())
+        .fallback_service(serve_dir);
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
